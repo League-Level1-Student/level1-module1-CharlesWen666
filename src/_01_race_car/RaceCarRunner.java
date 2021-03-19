bf215a9a39e528ab69fc768f955f095b4851081c
wench <wench@LAPTOP-MSGNLGK6.lan>
2021-03-19 16:59:17 -0700

    Weeeee

--- src/_01_race_car/RaceCarRunner.java
+++ src/_01_race_car/RaceCarRunner.java
@@ -12,12 +12,15 @@ public class RaceCarRunner {
 		
 		// 1. Create a RaceCar and place it in 5th position
 	
+		RaceCar car = new RaceCar("Lambo", 5);
 		// 2. Print the RaceCar's position in the race
-
+	System.out.println(car.getPositionInRace());
 		// 3. Crash the RaceCar
-		
+		car.crash();
 		// 4. If the car is damaged. Bring it in for a pit stop.
-
+	if(car.isDamaged()) {
+		car.pit();
+	}
 		// 5. Help the car move into first place.
 
 	}
---
+++ src/_02_cat/cat_runner.java
@@ -0,0 +1,10 @@
+package _02_cat;
+
+public class cat_runner {
+public static void main(String[] args) {
+	Cat Jerry = new Cat("Jerry");
+	Jerry.meow();
+	Jerry.printName();
+	Jerry.kill();
+}
+}
